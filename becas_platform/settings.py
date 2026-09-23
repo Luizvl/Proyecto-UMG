8661f60e09c8ef79e05fa9db92f322aa616b06cc
Joentolo <lopezkike255@gmail.com>
2026-09-22 19:42:31 -0600

    chore: configura MySQL mediante variables de entorno

--- becas_platform/settings.py
+++ becas_platform/settings.py
@@ -14,9 +14,10 @@ tocar código.
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(BASE_DIR / ".env")
 SECRET_KEY = os.environ.get(
     "DJANGO_SECRET_KEY",
     "django-insecure-CAMBIAR-ESTA-LLAVE-EN-PRODUCCION",
